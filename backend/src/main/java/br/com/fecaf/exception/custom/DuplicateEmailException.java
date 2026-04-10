9210bfecb72a3909638e7f7377de1838544eda98
pamela <pammellaxisto@gmail.com>
2026-04-10 14:50:59 -0300

    Ajustando o service para tratamento de erro

--- backend/src/main/java/br/com/fecaf/exception/custom/DuplicateEmailException.java
+++ backend/src/main/java/br/com/fecaf/exception/custom/DuplicateEmailException.java
@@ -1,6 +1,11 @@
 package br.com.fecaf.exception.custom;
 
 public class DuplicateEmailException extends RuntimeException {
+
+    public DuplicateEmailException(){
+        super("Email is already registered.");
+    }
+
     public DuplicateEmailException(String message) {
         super(message);
     }
--- backend/src/main/java/br/com/fecaf/repository/UserRepository.java
+++ backend/src/main/java/br/com/fecaf/repository/UserRepository.java
@@ -3,8 +3,6 @@ package br.com.fecaf.repository;
 import br.com.fecaf.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByCpf(String cpf);
